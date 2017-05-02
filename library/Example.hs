fe3ca046a599b1d397e6aa7be7b663216ac65d42
Leo Uno <leouno12@gmail.com>
2017-05-03 07:59:19 +0900

    add function "replace"

--- library/Example.hs
+++ library/Example.hs
@@ -1,5 +1,5 @@
 -- | An example module.
-module Example (main, hello) where
+module Example (main, hello, replace) where
 
 -- | An example function.
 main :: IO ()
@@ -7,3 +7,9 @@ main = return ()
 
 hello :: IO ()
 hello = putStrLn "hello"
+
+replace :: Int -> a -> [a] -> [a]
+replace n x xs = former ++ [x] ++ latter'
+    where
+        (former,latter) = splitAt n xs
+        latter' = tail latter
